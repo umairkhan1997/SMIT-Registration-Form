7441eef3eed949dc05abf77b4d682e7df2514dc8
Basit Ahmed <basitahmed1997@gmail.com>
2020-10-27 17:31:47 +0500

    Blood Bank Page Updated

--- src/components/SMIT/SmitContact.js
+++ src/components/SMIT/SmitContact.js
@@ -16,9 +16,7 @@ export default class SmitContact extends React.Component {
           style={{ backgroundColor: "white", marginTop: 80 }}
         >
           <div className="container d-flex justify-content-center">
-            <h1 className="Heading pt-3 text-center">
-              Contact Us
-            </h1>
+            <h1 className="Heading pt-3 text-center">Contact Us</h1>
           </div>
         </div>
 
@@ -26,58 +24,57 @@ export default class SmitContact extends React.Component {
           <div className="row">
             <div className="col-md-6">
               <div className="row">
-                <div className="col-md-5">
-                  <input placeholder="Name" class="form-control" />
+                <div className="col-md-6 pt-3 pb-3">
+                  <input placeholder="Name" class="inp" />
                 </div>
-                <div className="col-md-5">
-                  <input placeholder="Email" class="form-control" />
+                <div className="col-md-6 pt-3 pb-3">
+                  <input placeholder="Email" class="inp" />
                 </div>
-              </div>
-              <div>
-                <textarea
-                  cols="55"
-                  rows="10"
-                  placeholder="Message .."
-                  className="messageBoxTxt"
-                />
+                <div className="col-md-12 pt-3 pb-3">
+                  <textarea
+                    style={{ width: "100%" }}
+                    rows="8"
+                    placeholder="Message .."
+                    className="inp"
+                  />
+                </div>
+                <div className="col-md-12">
+                  <button style={{ width: "100%" }} className="donate">
+                    Submit
+                  </button>
+                </div>
               </div>
             </div>
             <div className="col-md-6 ">
-              <p className="text-left" style={{ color: "gray" }}>
-                {" "}
-                Saylani Mass IT Saylani Mass IT Saylani Mass IT Saylani Mass IT
-                Saylani Mass IT Saylani Mass IT Saylani Mass IT Saylani Mass IT
-                Saylani Mass IT Saylani Mass IT Saylani Mass IT Saylani Mass IT
-                Saylani Mass IT Saylani Mass IT Saylani Mass IT Saylani Mass IT
-                Saylani Mass IT Saylani Mass IT Saylani Mass IT
-              </p>
-              <div className="row" style={{ marginLeft: 10 }}>
-                <p style={{ fontSize: "1em" }}>
-                  <i
-                    class="fas fa-map-marker-alt"
-                    style={{ color: "gray" }}
-                  ></i>
-                </p>
-                <h6 style={{ width: 300, color: "gray" }}>
-                  4th Floor , A-25, Bahadurabad Chowrangi Karachi, Pakistan
-                </h6>
-              </div>
-              <div className="row" style={{ marginTop: 10, marginLeft: 10 }}>
-                <p style={{ fontSize: "1em" }}>
-                  <i class="fas fa-phone-alt" style={{ color: "gray" }}></i>
-                </p>
-                <h6 style={{ width: 200, color: "gray" }}>UAN: 111-729-526</h6>
-              </div>
-              <div className="row" style={{ marginTop: 0, marginLeft: 10 }}>
-                <p style={{ fontSize: "1em" }}>
-                  <i class="fas fa-envelope" style={{ color: "gray" }}></i>
-                </p>
-                <h6 style={{ width: 250, color: "gray" }}>
-                  info@saylaniwelfare.com
-                </h6>
-              </div>
-              <div className="d-flex justify-content-left">
-                <button className="btn btn-primary btn-lg">Submit</button>
+              <div className="row p-5">
+                <div className="col-md-1 text-right">
+                  <p>
+                    <i class="fas fa-map-marker-alt"></i>
+                  </p>
+                </div>
+                <div className="col-md-11 text-left">
+                  <h6>
+                    4th Floor , A-25, Bahadurabad Chowrangi Karachi, Pakistan
+                  </h6>
+                </div>
+
+                <div className="col-md-1 text-right">
+                  <p>
+                    <i class="fas fa-phone-alt"></i>
+                  </p>
+                </div>
+                <div className="col-md-11 text-left">
+                  <h6>UAN: 111-729-526</h6>
+                </div>
+
+                <div className="col-md-1 text-right">
+                  <p>
+                    <i class="fas fa-envelope"></i>
+                  </p>
+                </div>
+                <div className="col-md-11 text-left">
+                  <h6>education@saylaniwelfare.com</h6>
+                </div>
               </div>
             </div>
           </div>
--- src/components/bloodbank/bloodservices.js
+++ src/components/bloodbank/bloodservices.js
@@ -5,7 +5,7 @@ import "./blood.css";
 import logo from "../../images/logo.png";
 import Fade from "react-reveal";
 
-export default class BloodBankServices extends React.Component {
+export default class BloodBankAims extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -14,6 +14,9 @@ export default class BloodBankServices extends React.Component {
     return (
       <div>
         <div className="container pt-5 pb-5">
+          <div className='p-5'>
+            <h2 className='Heading'>Blood Bank Services</h2>
+          </div>
           <div className="row">
             <div className="col-md-3 p-3 mt-3 mb-3">
               <div className="shadow p-3">laboratory Services</div>
@@ -21,13 +24,23 @@ export default class BloodBankServices extends React.Component {
             <div className="col-md-3 p-3 mt-3 mb-3">
               <div className="shadow p-3">Blood Bank</div>
             </div>
-            <div className="col-md-3 p-3 mt-3 mb-3">Thalessimia Care</div>
-            <div className="col-md-3 p-3 mt-3 mb-3">General Physicians</div>
             <div className="col-md-3 p-3 mt-3 mb-3">
-              Bloods and its components Trandfusion
+              <div className="shadow p-3">Thalessimia Care</div>
             </div>
-            <div className="col-md-3 p-3 mt-3 mb-3">Effective Screening</div>
-            <div className="col-md-3 p-3 mt-3 mb-3">Medication</div>
+            <div className="col-md-3 p-3 mt-3 mb-3">
+              <div className="shadow p-3">General Physicians</div>
+            </div>
+            <div className="col-md-3 p-3 mt-3 mb-3">
+              <div className="shadow p-3">
+                Bloods and its components Trandfusion
+              </div>
+            </div>
+            <div className="col-md-3 p-3 mt-3 mb-3">
+              <div className="shadow p-3">Effective Screening</div>
+            </div>
+            <div className="col-md-3 p-3 mt-3 mb-3">
+              <div className="shadow p-3">Medication</div>
+            </div>
           </div>
         </div>
       </div>
