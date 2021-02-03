51073989827ccf839368292a2b1cffeeffe531ae
mumairkhan1997 <mohumairkhan47@gmail.com>
2021-02-03 13:06:40 +0500

    wednesay

--- src/components/Home/HomeNews.js
+++ src/components/Home/HomeNews.js
@@ -3,8 +3,23 @@ import "./Home.css";
 import img6 from "../../images/president.jpeg";
 import img2 from "../../images/govern.jpg";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  getAllNews,
+} from "../../Redux/action/homeAction";
+class HomeNews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+      limit: 5,
+    };
+  }
+  componentDidMount() {
+    const { page, limit } = this.state;
+    this.props.getAllNews(page, limit, "undefined");
+  }
 
-export default class HomeNews extends React.Component {
   render() {
     return (
       <div
@@ -17,19 +32,31 @@ export default class HomeNews extends React.Component {
           </div>
           <div className="row">
             <div className="col-md-6 px-4">
-              <div className="news row mt-2 mb-2 bg-white">
-                <div className="col-md-5 col-sm-3 py-3">
-                  <img src={img2} width="100%" />
-                </div>
-                <div className="col-md-7 col-sm-3 text-left">
-                  <p>Mr. Mohammad Sarwar Governer of Punjab </p>
-                  <p className="text-muted">
-                    On 21 Sep,2020 Hazrat Moulana Bashir Sahab Visit Punjab ...
-                  </p>
-                  <Link to="/news">Read More</Link>
-                </div>
-              </div>
-              <div className="news row mt-2 mb-2 bg-white">
+
+              {this.props.SaylaniNewsGet &&
+                this.props.SaylaniNewsGet.length > 0 &&
+                this.props.SaylaniNewsGet.slice(0, 3).map((data) => {
+                  //  console.log(data, 'first ')
+                  return (
+                    <div className="news row mt-2 mb-2 bg-white">
+                      <div className="col-md-5 col-sm-3 py-3">
+                        {data.multiple_image.slice(0, 1).map((each, index) => (
+
+                          <img key={index} src={each} width="100%" />
+                        ))}
+                      </div>
+                      <div className="col-md-7 col-sm-3 text-left">
+                        <p>{data.name}</p>
+                        <p className="text-muted">
+                          {data.description}
+                        </p>
+                        <Link to="/news">Read More</Link>
+                      </div>
+                    </div>
+                  );
+                })}
+
+              {/* <div className="news row mt-2 mb-2 bg-white">
                 <div className="col-md-5 col-sm-3 pt-3 pb-3">
                   <img src={img6} width="100%" />
                 </div>
@@ -53,7 +80,7 @@ export default class HomeNews extends React.Component {
                   </p>
                   <Link to="/news">Read More</Link>
                 </div>
-              </div>
+              </div> */}
             </div>
             <div className="col-md-6 p-2">
               <iframe
@@ -71,3 +98,19 @@ export default class HomeNews extends React.Component {
     );
   }
 }
+
+function mapStateToProp(state) {
+  return {
+    SaylaniNewsGet: state.root.SaylaniNewsGet,
+    SaylaniNewsNext: state.root.SaylaniNewsNext,
+  };
+}
+function mapDispatchToProp(dispatch) {
+  return {
+    getAllNews: (a, b, e) => {
+      console.log("dispatch is calling");
+      dispatch(getAllNews(a, b, e));
+    },
+  };
+}
+export default connect(mapStateToProp, mapDispatchToProp)(HomeNews);
